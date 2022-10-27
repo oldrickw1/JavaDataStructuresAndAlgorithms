a2dedf841ff94ff0866ba216e4550f4ea21ead5b
Oldrick Weenink <o.weenink@hotmail.com>
2022-10-27 18:28:26 +0200

    Refactored unit tests so that the test methods use the .insert method for setting up test cases

--- src/test/java/ArrayTest.java
+++ src/test/java/ArrayTest.java
@@ -84,11 +84,10 @@ class ArrayTest {
     @Test
     @DisplayName("First item gets removed")
     void removeFirstItem() {
-        underTest.items[0] = 1;
-        underTest.items[1] = 2;
-        underTest.items[2] = 3;
-        underTest.items[3] = 4;
-        underTest.count = 4;
+        underTest.insert(1);
+        underTest.insert(2);
+        underTest.insert(3);
+        underTest.insert(4);
         underTest.removeAt(0);
         assertEquals(2, underTest.items[0]);
         assertEquals(3, underTest.items[1]);
@@ -98,11 +97,10 @@ class ArrayTest {
     @Test
     @DisplayName("Last item gets removed")
     void removeLastItem() {
-        underTest.items[0] = 1;
-        underTest.items[1] = 2;
-        underTest.items[2] = 3;
-        underTest.items[3] = 4;
-        underTest.count = 4;
+        underTest.insert(1);
+        underTest.insert(2);
+        underTest.insert(3);
+        underTest.insert(4);
         underTest.removeAt(3);
         assertEquals(1, underTest.items[0]);
         assertEquals(2, underTest.items[1]);
@@ -113,11 +111,10 @@ class ArrayTest {
     @Test
     @DisplayName("Item somewhere in the middle gets removed")
     void removeMiddleItem() {
-        underTest.items[0] = 1;
-        underTest.items[1] = 2;
-        underTest.items[2] = 3;
-        underTest.items[3] = 4;
-        underTest.count = 4;
+        underTest.insert(1);
+        underTest.insert(2);
+        underTest.insert(3);
+        underTest.insert(4);
         underTest.removeAt(2);
         assertEquals(1, underTest.items[0]);
         assertEquals(2, underTest.items[1]);
@@ -127,11 +124,10 @@ class ArrayTest {
     @Test
     @DisplayName("Trying to remove an index out of bound raises an exception")
     void removeItemOutOfBound() {
-        underTest.items[0] = 1;
-        underTest.items[1] = 2;
-        underTest.items[2] = 3;
-        underTest.items[3] = 4;
-        underTest.count = 4;
+        underTest.insert(1);
+        underTest.insert(2);
+        underTest.insert(3);
+        underTest.insert(4);
         assertThrows(IndexOutOfBoundsException.class, () -> {
             underTest.removeAt(5);
         });
@@ -148,11 +144,10 @@ class ArrayTest {
     @Test
     @DisplayName("Removed item should be returned")
     void returnRemovedItem() {
-        underTest.items[0] = 1;
-        underTest.items[1] = 2;
-        underTest.items[2] = 3;
-        underTest.items[3] = 4;
-        underTest.count = 4;
+        underTest.insert(1);
+        underTest.insert(2);
+        underTest.insert(3);
+        underTest.insert(4);
         int removedItem = underTest.removeAt(3);
         assertEquals(4, removedItem);
     }
@@ -167,11 +162,10 @@ class ArrayTest {
     @Test
     @DisplayName("Prints an array")
     void printsAnArray() {
-        underTest.items[0] = 1;
-        underTest.items[1] = 2;
-        underTest.items[2] = 3;
-        underTest.items[3] = 4;
-        underTest.count = 4;
+        underTest.insert(1);
+        underTest.insert(2);
+        underTest.insert(3);
+        underTest.insert(4);
         assertEquals("[1, 2, 3, 4]", underTest.print());
     }
 
@@ -180,58 +174,56 @@ class ArrayTest {
     @Test
     @DisplayName("Provides index of item that is in the array at the first index")
     void itemExistsAtFirstIndex() {
-        underTest.items[0] = 1;
-        underTest.items[1] = 2;
-        underTest.items[2] = 3;
-        underTest.items[3] = 4;
-        underTest.count = 4;
-
+        underTest.insert(1);
+        underTest.insert(2);
+        underTest.insert(3);
+        underTest.insert(4);
         assertEquals(0, underTest.indexOf(1));
     }
 
     @Test
     @DisplayName("Provides index of item that is in the array at the last index")
     void itemExistAtTheLastIndex() {
-        underTest.items[0] = 1;
-        underTest.items[1] = 2;
-        underTest.items[2] = 3;
-        underTest.items[3] = 4;
-        underTest.count = 4;
+        underTest.insert(1);
+        underTest.insert(2);
+        underTest.insert(3);
+        underTest.insert(4);
         assertEquals(3, underTest.indexOf(4));
     }
 
     @Test
     @DisplayName("Provides index of item that is in the array at a middle index")
     void itemExistAtAMiddleIndex() {
-        underTest.items[0] = 1;
-        underTest.items[1] = 2;
-        underTest.items[2] = 3;
-        underTest.items[3] = 4;
-        underTest.count = 4;
+        underTest.insert(1);
+        underTest.insert(2);
+        underTest.insert(3);
+        underTest.insert(4);
         assertEquals(1, underTest.indexOf(2));
     }
 
     @Test
     @DisplayName("Provides first index if item appears multiple times")
     void firstIndexMultipleAppearances() {
-        underTest.items[0] = 1;
-        underTest.items[1] = 2;
-        underTest.items[2] = 2;
-        underTest.items[3] = 2;
-        underTest.count = 4;
+        underTest.insert(1);
+        underTest.insert(2);
+        underTest.insert(2);
+        underTest.insert(2);
         assertEquals(1, underTest.indexOf(2));
     }
 
     @Test
     @DisplayName("Returns -1 if item not in array")
     void itemNotInArray() {
-        underTest.items[0] = 1;
-        underTest.items[1] = 2;
-        underTest.items[2] = 2;
-        underTest.items[3] = 2;
-        underTest.count = 4;
+        underTest.insert(1);
+        underTest.insert(2);
+        underTest.insert(2);
+        underTest.insert(2);
         assertEquals(-1, underTest.indexOf(10));
     }
+
+
+
+
 
 
 }
