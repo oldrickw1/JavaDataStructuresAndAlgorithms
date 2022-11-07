94221fe16cc2d6cfa101426d0785f1f1d0f001da
Oldrick Weenink <o.weenink@hotmail.com>
2022-11-07 17:45:55 +0100

    Added some test methods

--- src/test/java/LinkedListTest.java
+++ src/test/java/LinkedListTest.java
@@ -53,12 +53,49 @@ public class LinkedListTest {
         });
     }
 
-//    // Testing getSize method
-//    @Test
-//    @DisplayName()
+    // Testing getSize method
+    @Test
+    @DisplayName("Returns the correct size")
+    void returnsSize() {
+        assertEquals(0, underTest.getSize());
+        underTest.addFirst(1);
+        underTest.addFirst(2);
+        assertEquals(2, underTest.getSize());
+    }
 
     // Testing addLast method
+    @Test
+    @DisplayName("Adding a last node to an empty list")
+    void addLastToEmptyLinkedList() {
+        underTest.addLast(4);
+        assertEquals(1, underTest.getSize());
+        assertEquals("[4]", underTest.print());
+        assertEquals(4, underTest.get(0));
+    }
+
+    void addLastToLinkedList() {
+        underTest.addFirst(4);
+        underTest.addLast(5);
+        assertEquals(2, underTest.getSize());
+        assertEquals("[4,5]", underTest.print());
+        assertEquals(4, underTest.get(0));
+        assertEquals(5, underTest.get(1));
+    }
+
+    void addMultipleLastItems() {
+        underTest.addLast(3);
+        underTest.addLast(5);
+        underTest.addLast(7);
+        assertEquals(7, underTest.get(2));
+        assertEquals(3, underTest.get(0));
+        assertEquals("[3,5,7]", underTest.print());
+        assertEquals(3, underTest.getSize());
+    }
+
+
+
     // Testing deleteFirst method
+
     // Testing deleteLast method
     // Testing contains method
     // Testing indexOf method
